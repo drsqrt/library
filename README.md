1304b9a651eb4befd7338115cc64cfb4614e203a
Ayush Rawat <ayushrawat3338@gmail.com>
2025-01-05 01:09:57 +0530

    pivot to handling resources

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ Ideas : Create backend for persistence
       : Search in files
       : Handling of recursive rendering efficiently
       : Configure iosevka font
+
+Pivot : I often find very insightful resources online in form of article(html), research papers (pdfs), books (pdfs), and many other
+      : Basically my own way of bookmarking resources such that i can find that on every single device i am on without much configurations...
+      : All the resources will be having the credits to the original author.
+      : I don't own any of these resources, they are just the way of storing all of them in a proper organized way and for effecient searching.
 ```
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,52 @@
-import './styles/Home.css'
+import { useEffect, useState } from "react";
+import "./styles/Home.css";
+
+const url = "https://raw.githubusercontent.com/drsqrt/files/refs/heads/main/resources/resources.json";
+
+type FileType = {
+  id: number;
+  name: string;
+  url: string;
+  folderId: number;
+};
+
+type FolderType = {
+  id: number;
+  name: string;
+  parentId: number;
+};
+
 function Home() {
+  const [files, setFiles] = useState<FileType[] | null>(null);
+  const [folders, setFolders] = useState<FolderType[] | null>(null);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      setFiles(json.files);
+      setFolders(json.folders);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <div className="home">
-        <h1>Home Component</h1>
+    <div className="home">
+      <div className="left-panel">
+        <menu>
+          <li>Articles</li>
+          <li>Books</li>
+          <li>Papers</li>
+        </menu>
       </div>
-    </>
+    </div>
   );
 }
 export default Home;
