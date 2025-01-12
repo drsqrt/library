8916ea6ffbedddf9a12433d111ced89b7a05e4f2
Ayush Rawat <ayushrawat3338@gmail.com>
2025-01-12 11:30:58 +0530

    iosevka font

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./reset.css";
 import "./main.css";
+import '@fontsource/iosevka';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
